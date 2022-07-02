ed8981b6543766d4ba1300202d48920ffa9b14be
Rossana Silva <rlaurentino96@gmail.com>
2022-07-01 23:33:35 -0300

    tudo5

--- angular-cli-libs-externas/src/app/app.module.ts
+++ angular-cli-libs-externas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
     BrowserModule,
